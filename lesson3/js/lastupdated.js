6cbc85bf08031009615c9cc44e62300768afe1fd
sistersonya <sistersonya@icloud.com>
2020-10-02 13:35:03 -0600

    Web Design Document

---
+++ lesson3/js/lastupdated.js
@@ -0,0 +1,8 @@
+// function myFunction() {
+//     var x = document.lastModified;
+//     document.getElementById("demo").innerHTML = x;
+//   }
+// document.write("Last Updated: " + document.lastModified +""); 
+document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified +"";
+
+// http://www.web-source.net/web_design_tips/last_modified_date_web_page_script.htm#.XqCiafl7lEY
